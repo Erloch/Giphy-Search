1f13a724128e82dd5609a75810f128ad6da25ddc
Erich B <erichbernhardt@hotmail.com>
2019-06-13 11:41:21 -0600

    completed assignment

---
+++ assets/giphy.js
@@ -0,0 +1,10 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+var div = $("<div>");
+var gifBtn = $("<button>" + search + "</button>");
+for (var i = 0; i < array.length; i++){
+  
+}
